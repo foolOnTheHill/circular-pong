d6b95a0321a49a0b582666eafe2edd72825fa3a8
George H. Alvares <blackbird.fly95@gmail.com>
2015-05-07 21:20:13 -0300

    repositioning the loading sprite

--- js/load.js
+++ js/load.js
@@ -12,10 +12,14 @@ var load = {
 	preload: function() {
 		this.fac = this.game.world.width/650;
 
-		preloading2 = this.game.add.sprite(this.game.world.width/2-(50*this.fac), this.game.world.height/2, 'loading2');
+		preloading2 = this.game.add.sprite(this.game.world.width/2, this.game.world.height/2, 'loading2');
 		preloading2.x -= (preloading2.width*this.fac)/2;
-		preloading = this.game.add.sprite(this.game.world.width/2-(50*this.fac), this.game.world.height/2+(4*this.fac), 'loading');
+		preloading2.scale.setTo(this.fac, this.fac);
+
+		preloading = this.game.add.sprite((this.game.world.width/2), (this.game.world.height/2)+4, 'loading');
 		preloading.x -= (preloading.width*this.fac)/2;
+		preloading.scale.setTo(this.fac, this.fac);
+
 		this.game.load.setPreloadSprite(preloading);
 
 		this.game.load.image('pallete', 'assets/purple_brick.png');

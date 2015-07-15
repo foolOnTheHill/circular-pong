7b2bb69e34e9499d0a9e4439d02501fe3938db0c
George H. Alvares <blackbird.fly95@gmail.com>
2015-07-15 01:22:16 -0300

    better collision handling

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@ var main = {
 		// Game Stage
 		var graphics = this.game.add.graphics(this.game.world.centerX, this.game.world.centerY);
 		graphics.lineStyle(0);
-	    graphics.beginFill(0x4099ff, 0.7);
-	    graphics.drawCircle(325/128, 325/128, 490*this.resize);
-	    graphics.endFill();
-	
+    graphics.beginFill(0x4099ff, 0.7);
+    graphics.drawCircle(325/128, 325/128, 490*this.resize);
+    graphics.endFill();
+
 		// Game Constants
 		this.direction = 1;
 		this.radius = 250*this.resize;
@@ -38,7 +38,7 @@ var main = {
 			this.scoreY = 40;
 		}
 
-	    // Game Objects
+	  // Game Objects
 		this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + this.radius, 'pallete');
 		this.player.scale.setTo(this.resize, this.resize);
 		this.game.physics.arcade.enable(this.player);
@@ -68,7 +68,7 @@ var main = {
 
 		// Instructions
 		size = 40*this.resize;
-		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) { 	
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 			this.instructions = this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Tap to control the pad.', {font: size+'px "Righteous"', fill: '#f6546a'});
 		} else {
 			this.instructions = this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Press \'left\' and \'right\' or tap\nto control the pad', {font: size+'px "Righteous"', fill: '#f6546a', align: 'center'});
@@ -78,7 +78,7 @@ var main = {
 
 	update: function() {
 
-		if ((this.cursors.left.isDown || this.cursors.right.isDown || this.game.input.mousePointer.isDown || this.game.input.pointer1.isDown) && !this.ballMoving) {
+		if (!this.ballMoving && (this.cursors.left.isDown || this.cursors.right.isDown || this.game.input.mousePointer.isDown || this.game.input.pointer1.isDown)) {
 			this.instructions.destroy();
 			this.ball.visible = true;
 			this.ball.body.velocity.x = 315*this.resize;
@@ -88,7 +88,7 @@ var main = {
 		}
 
 		if (this.cursors.left.isDown && this.direction === -1) {
-			this.direction = 1; 
+			this.direction = 1;
 		} else if (this.cursors.right.isDown && this.direction === 1) {
 			this.direction = -1;
 		} else if ((this.game.input.mousePointer.isDown || this.game.input.pointer1.isDown) && this.game.time.now > this.mouseTime ) {
@@ -96,29 +96,48 @@ var main = {
 			this.mouseTime = this.game.time.now + this.mouseDelta;
 		}
 
-		if (!this.checkBall()) {
-			this.player.kill();
-			this.game.higher = this.higher;
-			if (this.higher) window.localStorage.setItem('pong', this.game.score);
-			this.game.state.start('end');
-		} else if (this.enablePad) {
+		this.handleCollisions();
+	},
+
+	handleCollisions: function() {
+		var check = this.checkBall();
+		var threshold = this.ball.width+this.player.height;
+
+		if (!check) {
+			var playerX = this.player.x;
+			var playerY = this.player.y;
+			var angle = this.player.rotation;
+
+			var a = -Math.tan(this.period);
+			var b = playerY - a*playerX;
+
+			var diff = Math.abs( (a*this.ball.x + b) - this.ball.y );
+
+			if (diff <= threshold) {
+				this.hitBall();
+			} else {
+				this.handleGameOver();
+			}
+		} else if (this.enablePad){
 			this.circularMotion();
-			this.game.physics.arcade.overlap(this.player, this.ball, null, this.hitBall, this);
 		}
+	},
+
+	handleGameOver: function() {
+		this.player.kill();
+		this.game.higher = this.higher;
+		if (this.higher) window.localStorage.setItem('pong', this.game.score);
+		this.game.state.start('end');
 	},
 
 	checkBall: function() {
+		var r = this.radius - this.ball.width/2;
 		var dx = (this.ball.x - this.game.world.centerX)*(this.ball.x - this.game.world.centerX);
 		var dy = (this.ball.y - this.game.world.centerY)*(this.ball.y - this.game.world.centerY);
-		return (dx + dy) <= (this.radius*this.radius) + (this.resize*2500);
+		return (dx + dy) <= r*r;
 	},
 
 	circularMotion: function () {
-
-		if (this.player === undefined) {
-			console.log('Dead');
-		}
-
 		if (this.direction === -1) {
 			this.period = (this.period - 0.08)%360;
 		} else {
@@ -127,25 +146,25 @@ var main = {
 
 		this.player.x = this.game.world.centerX + Math.cos(this.period)*this.radius;
 		this.player.y = this.game.world.centerY + Math.sin(this.period)*this.radius;
-		
+
 		this.player.rotation = this.period - 0.5*Math.PI;
 	},
 
-	hitBall: function(player, ball) {
+	hitBall: function() {
 		if (this.game.time.now < this.hitTime) return;
 
 		var fac = 0.85;
 		var velX = this.game.world.centerX - this.player.x;
 		var velY = this.game.world.centerY - this.player.y;
 
-		ball.body.velocity.x = (-ball.body.velocity.x)*(1-fac) + velX*fac;
-		ball.body.velocity.y = (-ball.body.velocity.y)*(1-fac) + velY*fac;
+		this.ball.body.velocity.x = (-this.ball.body.velocity.x)*(1-fac) + velX*fac;
+		this.ball.body.velocity.y = (-this.ball.body.velocity.y)*(1-fac) + velY*fac;
 
-		ball.body.velocity.x *= 1 + Math.random();
-		ball.body.velocity.y *= 1 + Math.random();		
+		this.ball.body.velocity.x *= 1 + Math.random();
+		this.ball.body.velocity.y *= 1 + Math.random();
 
-		this.game.add.tween(ball.scale).to({x:1.3*this.resize, y:1.3*this.resize}, 200).to({x:0.9*this.resize, y:0.9*this.resize}, 200).start();
-		
+		this.game.add.tween(this.ball.scale).to({x:1.3*this.resize, y:1.3*this.resize}, 200).to({x:0.9*this.resize, y:0.9*this.resize}, 200).start();
+
 		this.hit_sound.play();
 		this.updateScore();
 
@@ -158,7 +177,7 @@ var main = {
 		if (this.game.score > this.highScore && !this.higher) {
 			this.higher = true;
 			this.highScore_text = this.game.add.text(this.game.world.centerX, this.scoreY + 32, 'highscore!', {font: '2em "Righteous"', fill: '#f6546a'});
-			this.highScore_text.anchor.setTo(0.5, 0.5)		
+			this.highScore_text.anchor.setTo(0.5, 0.5)
 			this.game.add.tween(this.highScore_text).to({ angle:1 }, 200).to({ angle:-1 }, 200).loop().start();
 		}
 
